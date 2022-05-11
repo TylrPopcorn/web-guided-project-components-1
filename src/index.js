68e97e2012832c5e674539f313c0bf923b852a98
Casey Harding <casey.r.harding@gmail.com>
2022-05-11 13:19:20 -0400

    Through to task 10.

--- src/index.js
+++ src/index.js
@@ -75,8 +75,6 @@ const accordion = document.querySelector(".accordion");
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
 function makePanel({ title, content }) {
-
-
   // TASK 5- Instantiate all the elements needed for a panel
   const panel = document.createElement("div");
   const panelBar = document.createElement("div");
@@ -85,7 +83,6 @@ function makePanel({ title, content }) {
   const panelButtons = document.createElement("div");
   const openButton = document.createElement("button");
   const closeButton = document.createElement("button");
-
 
   // TASK 6- Setup the structure of our elements
   /*
@@ -100,49 +97,46 @@ function makePanel({ title, content }) {
       <div></div>           // panelContent
     </div>
   */
-  panel.appendChild(panelBar);
+  panel.appendChild(panelBar); //<div><div></div></div>
   panel.appendChild(panelContent);
   panelBar.appendChild(panelTitle);
   panelBar.appendChild(panelButtons);
   panelButtons.appendChild(openButton);
   panelButtons.appendChild(closeButton);
 
+  // What is a component -> repetitive markup...generates the markup...
+
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
-
-  /**
-   *    <!-- Remove this -->
-        <div class="panel">
-          <div class="panel-bar">
-            <h3>Title of Panel</h3>
-            <div class="panel-buttons">
-              <button class="panel-btn-open">&#9660</button>
-              <button class="panel-btn-close hide-btn">Close</button>
-            </div>
-          </div>
-          <div class="panel-content toggle-on">
-            Content of panel
-          </div>
-        </div>
-        <!-- Remove this -->
-   */
-
+  panel.classList.add("panel");
+  panelBar.classList.add("panel-bar");
+  panelButtons.classList.add("panel-buttons");
+  openButton.classList.add("panel-btn-open");
+  closeButton.classList.add("panel-btn-close", "hide-btn");
+  panelContent.classList.add("panel-content");
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
+  panelTitle.textContent = title;
+  panelContent.textContent = content;
+  openButton.textContent = open;
+  closeButton.textContent = close;
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
   //  - the close button needs to show (the 'hide-btn' class name controls this)
   //  - the contents need to show (the 'toggle-on' class name controls this)
-
+  panelButtons.addEventListener("click", () => {
+    openButton.classList.toggle("hide-btn");
+    closeButton.classList.toggle("hide-btn");
+    panelContent.classList.toggle("toggle-on");
+  })
 
   // don't forget to return the panel!
-  return null
+  return panel;
 }
-const testPanel = makePanel({ title: "Lorem", content: "Ipsum " });
-
+// const testPanel = makePanel({ title: "Lorem", content: "Ipsum " });
+// accordion.appendChild(testPanel);
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
